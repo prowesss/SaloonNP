78d3169c7d3f55ad58ce22aa7a8d860a44d22a50
prowesspradhan@gmail.com <prowesspradhan@gmail.com>
2023-03-07 22:13:01 +1300

    Create a MVC project with authentication

---
+++ SaloonNP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SaloonNP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
