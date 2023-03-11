e88792be83fbf264f848d0a6b074713762d8ef75
prowesspradhan@gmail.com <prowesspradhan@gmail.com>
2023-03-12 00:12:46 +1300

    added controller view and model for hairstyle and location and made correction in staff_hairstyle relationship using new migration

---
+++ SaloonNP/Controllers/HairstyleController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SaloonNP.Data;
+using SaloonNP.Models.ServiceManagementModels;
+using SaloonNP.Models.UserManagementModels;
+using SaloonNP.Models.ViewModels.HairStyle;
+
+
+namespace SaloonNP.Controllers
+{
+    public class HairstyleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HairstyleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var hairstyle = await _context.HairStyle.ToListAsync();
+                return View(hairstyle);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<HairStyle> Create()
+        {
+            var staff = _context.Staff.ToList();
+            var staffSelectList = new List<SelectListItem>();
+            foreach (var item in staff)
+            {
+                staffSelectList.Add(new SelectListItem() { Text = item.FullName, Value = item.Id.ToString() });
+            }
+
+            ViewBag.StaffIds = staffSelectList;
+            var location = _context.Location;
+            ViewBag.Location = new SelectList(location, "Id", "Address");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HairstyleViewModel addHairstyle)
+        {
+            if (ModelState.IsValid)
+            {
+                var location = await _context.Location.FirstOrDefaultAsync(x => x.Id == addHairstyle.LocationId);
+                
+                HairStyle newHairstyle = new HairStyle()
+                {
+                    Name = addHairstyle.Name,
+                    Description = addHairstyle.Description,
+                    Price = addHairstyle.Price,
+                    ImageURL = addHairstyle.ImageURL,
+                    Gender = addHairstyle.Gender,
+                    Location = location
+                };
+
+                _context.HairStyle.Add(newHairstyle);
+                await _context.SaveChangesAsync();
+                
+                var staffs = new List<Staff_HairStyle>();
+
+                if (addHairstyle.StaffIds.Any())
+                {
+                    foreach (var item in addHairstyle.StaffIds)
+                    {
+                        var staff = await _context.Staff.FirstOrDefaultAsync(x => x.Id == item);
+                        if (staff != null)
+                        {
+                            staffs.Add(new Staff_HairStyle { Staff = staff , HairStyle = newHairstyle});
+                        }
+                    }
+
+                }
+                _context.Staff_HairStyle.AddRange(staffs);
+                await _context.SaveChangesAsync();
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addHairstyle);
+        }
+    }
+}
---
+++ SaloonNP/Models/ViewModels/HairStyle/HairstyleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SaloonNP.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaloonNP.Models.ViewModels.HairStyle
+{
+    public class HairstyleViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double? Price { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public List<Guid> StaffIds { get; set; }
+
+        public Guid LocationId { get; set; }
+
+        
+
+    }
+}
