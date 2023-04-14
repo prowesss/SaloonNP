a9cff2756ee8435c22e2a4147e776bfd2eaef54a
prowesss <39946326+prowesss@users.noreply.github.com>
2023-04-14 17:48:17 +1200

    Delete Saloon directory

--- Saloon/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Saloon.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
