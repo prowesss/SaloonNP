d0205c42a9fda0c4865ba767ce0f21d12f20d204
prowesss <39946326+prowesss@users.noreply.github.com>
2023-04-14 19:15:40 +1200

    Create AppointmentViewModel.cs

--- SaloonNP/Models/ViewModels/Appointment/AppointmentViewModel.cs
+++ SaloonNP/Models/ViewModels/Appointment/AppointmentViewModel.cs
@@ -6,13 +6,13 @@ namespace SaloonNP.Models.ViewModels.Appointment
     public class AppointmentViewModel
     {
         public Guid? Id { get; set; }
-        public DateTime AppointmentDate { get; set; }
+        public DateTime appointmentdate { get; set; }
         public string Notes { get; set; }
         public bool IsCancelled { get; set; }
         public string ProductIds { get; set; }
         public Guid StaffId { get; set; }
         public Guid LocationId { get; set; }
-        public Guid HairstyleId { get; set; }
+        public Guid hairstyleId { get; set; }
         public IEnumerable<SaloonNP.Models.ServiceManagementModels.HairStyle>? HairStyles { get; set; }
         public List<SaloonNP.Models.ServiceManagementModels.Product>? Products { get; set; }
         public List<SelectListItem>? Locations { get; set; }
