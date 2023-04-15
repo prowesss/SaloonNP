19c33d131cab0212e94c0275280287c2f0c9a4d0
umesheo <52323325+umesheo@users.noreply.github.com>
2023-04-15 23:08:25 +1200

    Update AppointmentViewModel.cs

--- SaloonNP/Models/ViewModels/Appointment/AppointmentViewModel.cs
+++ SaloonNP/Models/ViewModels/Appointment/AppointmentViewModel.cs
@@ -6,12 +6,14 @@ namespace SaloonNP.Models.ViewModels.Appointment
     public class AppointmentViewModel
     {
         public Guid? Id { get; set; }
+        //Reviewer Umesh 20220860 : DateTime appointmentdate varibale should be AppointmentDate 
         public DateTime appointmentdate { get; set; }
         public string Notes { get; set; }
         public bool IsCancelled { get; set; }
         public string ProductIds { get; set; }
         public Guid StaffId { get; set; }
         public Guid LocationId { get; set; }
+        //Reviewer Umesh 20220860: hairstyleId should be declared as HairStyleId
         public Guid hairstyleId { get; set; }
         public IEnumerable<SaloonNP.Models.ServiceManagementModels.HairStyle>? HairStyles { get; set; }
         public List<SaloonNP.Models.ServiceManagementModels.Product>? Products { get; set; }
